93fb286df0de1eb26b5c562ca0547828bf291059
1e1 <1e1@users.noreply.github.com>
2024-10-29 10:23:23 +0100

    fix TimestampNtp.hpp
    tests:
    ```
    1704067200 = 2024-01-01T00:00:00Z => 2017-13-46T00:00:00Z
    
    1709251200 = 2024-03-01T00:00:00Z => 2024-03-01T00:00:00Z
    
    2934835200 = 2063-01-01T00:00:00Z => 2063-01-01T00:00:00Z
    
    2713910400 = 2056-01-01T00:00:00Z => 2056-01-01T00:00:00Z
    
    3723753600 = 2088-01-01T00:00:00Z => 2088-01-01T00:00:00Z
    
    1709334000 = 2024-03-01T23:00:00Z => 2024-03-01T23:00:00Z
    
    1709337540 = 2024-03-01T23:59:00Z => 2024-03-01T23:59:00Z
    
    1709337599 = 2024-03-01T23:59:59Z => 2024-03-01T23:59:00Z
    
    3729020400 = 2088-03-01T23:00:00Z => 2088-03-01T23:00:00Z
    
    3729020400 = 2088-03-01T23:59:00Z => 2088-03-01T23:00:00Z
    
    3729023999 = 2088-03-01T23:59:59Z => 2088-03-01T23:59:00Z
    
    4070908800 = 2099-01-01T00:00:00Z => 2099-01-01T00:00:00Z
    
    4102358399 = 2099-12-30T23:59:59Z => 2099-12-30T23:59:00Z
    
    4102358400 = 2099-12-31T00:00:00Z => 2099-12-31T00:00:00Z
    
    4102444799 = 2099-12-31T23:59:59Z => 2099-12-31T23:59:00Z
    
    1735603199 = 2024-12-30T23:59:59Z => 2024-12-30T23:59:00Z
    
    1735603200 = 2024-12-31T00:00:00Z => 2024-12-31T00:00:00Z
    
    1735689599 = 2024-12-31T23:59:59Z => 2024-12-31T23:59:00Z
    
    ```

--- examples/offline/offline.ino
+++ examples/offline/offline.ino
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-#include "FastTimer.hpp"
+#include <FastTimer.hpp>
 
 
 bool isLedOn; 
@@ -10,7 +10,7 @@ FastTimer<FastTimer_precision_t::P_1s_4m> timer1s;
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);    
+    digitalWrite(LED_BUILTIN, HIGH);    
     isLedOn = false;
 }
 
@@ -21,9 +21,9 @@ void loop()
     if (timer1s.isTick()) {
         isLedOn = !isLedOn;
         if (isLedOn) {
-            digitalWrite(LED_BUILTIN, HIGH);
-        } else {
             digitalWrite(LED_BUILTIN, LOW);
+        } else {
+            digitalWrite(LED_BUILTIN, HIGH);
         }
     }
 
